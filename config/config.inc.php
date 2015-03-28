76fb03f8937a855ff2411010770f4cfda616ae12
Matheus Xavier Silva <matheus_xavier-silva@live.com>
2015-03-28 19:24:12 -0300

    just started the works

---
+++ config/config.inc.php
@@ -0,0 +1,7 @@
+<!-- options file don't change unless you know what you are doing -->
+<?php
+/*The default database is mysql but it can be changed at the setup*/
+// Database connection info
+$dbcs = "";
+$dbuser = "";
+$dbpass = "";
